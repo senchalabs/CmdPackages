2ddc1eb0a83957a74c46056d0fdffd244ac44e1b
Philip Guerrant <pguerrant@gmail.com>
2013-11-05 16:04:41 -0700

    bump version

--- packages/ext-easy-button/examples/easy-button-demo/bootstrap.js
+++ packages/ext-easy-button/examples/easy-button-demo/bootstrap.js
@@ -1728,7 +1728,7 @@ Ext.ClassManager.addNameAliasMappings({
   "ExtThemeNeptune.toolbar.Paging": [],
   "ExtThemeNeptune.toolbar.Toolbar": []
 });
-Ext.setVersion("easy-button", "1.0.0");
+Ext.setVersion("ext-easy-button", "1.0.1");
 Ext.setVersion("ext-theme-base", "4.2.1");
 Ext.setVersion("ext-theme-neptune", "4.2.1");
 Ext.setVersion("ext-theme-neutral", "4.2.1");
